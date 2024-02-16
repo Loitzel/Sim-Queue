044b627228c6c3c8ff97f6e43d47602e04315961
karendcl <karen.canterolopez@gmail.com>
2024-02-15 22:46:37 -0500

    printing average, std deviation, max, min, 75th percentile

--- sim.py
+++ sim.py
@@ -101,7 +101,7 @@ class Simulation:
             self.handle_arrivals(self.clock)
             self.service(self.clock)
             self.clock += 1
-        print("Simulation finished")
+        #print("Simulation finished")
 
     def handle_arrivals(self, instance_time):
         """
@@ -133,29 +133,63 @@ class Simulation:
         Print stats
         """
 
-        print("Simulation stats")
-        print(f"Customers arrived: {self.cust_arrived}")
-        print(f"Customers served: {self.cust_served}")
-        print(f"Customers left in queue: {self.queue.qsize()}")
-        print(f"Average waiting time: {self.waiting_time/self.cust_served}")
+        # print("Simulation stats")
+        # print(f"Customers arrived: {self.cust_arrived}")
+        # print(f"Customers served: {self.cust_served}")
+        # print(f"Customers left in queue: {self.queue.qsize()}")
+        # print(f"Average waiting time: {self.waiting_time/self.cust_served}")
+
+        return self.cust_arrived, self.cust_served, self.queue.qsize(), self.waiting_time/self.cust_served
 
 
 
 
-#Testing
-arrival_dist = "poisson"
-lambda_arrival = 2
+def Test():
+    arrival_dist = "poisson"
+    lambda_arrival = 2
 
-server_1 = Server("exponential", 1, 1)
-server_2 = Server("uniform", 2, 1, 3)
-server_3 = Server("normal", 3, 5, 2)
+    server_1 = Server("exponential", 1, 1)
+    server_2 = Server("uniform", 2, 1, 3)
+    server_3 = Server("normal", 3, 5, 2)
 
-servers = [server_1, server_2, server_3]
+    servers = [server_1, server_2, server_3]
+
+    #run simulation
+    sim = Simulation(arrival_dist, lambda_arrival, servers=servers, simulation_time=60*8)
+    sim.run()
+    return sim.Stats()
 
 
+def Totals():
+    results = []
+    for i in range(100):
+        results.append(Test())
+    results = np.array(results)
+    print(f"Average customers arrived: {np.mean(results[:,0])}")
+    print(f"Average customers served: {np.mean(results[:,1])}")
+    print(f"Average customers left in queue: {np.mean(results[:,2])}")
+    print(f"Average waiting time: {np.mean(results[:,3])} \n")
 
-#run simulation
-sim = Simulation(arrival_dist, lambda_arrival, servers=servers, simulation_time=60*8)
-sim.run()
-sim.Stats()
+    print(f"Standard deviation customers arrived: {np.std(results[:,0])}")
+    print(f"Standard deviation customers served: {np.std(results[:,1])}")
+    print(f"Standard deviation customers left in queue: {np.std(results[:,2])}")
+    print(f"Standard deviation waiting time: {np.std(results[:,3])}\n")
 
+    print(f"Max customers arrived: {np.max(results[:,0])}")
+    print(f"Max customers served: {np.max(results[:,1])}")
+    print(f"Max customers left in queue: {np.max(results[:,2])}")
+    print(f"Max waiting time: {np.max(results[:,3])}\n")
+
+    print(f"Min customers arrived: {np.min(results[:,0])}")
+    print(f"Min customers served: {np.min(results[:,1])}")
+    print(f"Min customers left in queue: {np.min(results[:,2])}")
+    print(f"Min waiting time: {np.min(results[:,3])}\n")
+
+    print(f"75% confidence interval customers arrived: {np.percentile(results[:,0], [25, 75])}")
+    print(f"75% confidence interval customers served: {np.percentile(results[:,1], [25, 75])}")
+    print(f"75% confidence interval customers left in queue: {np.percentile(results[:,2], [25, 75])}")
+    print(f"75% confidence interval waiting time: {np.percentile(results[:,3], [25, 75])}\n")
+
+
+if __name__ == "__main__":
+    Totals()
